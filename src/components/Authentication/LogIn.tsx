f6155b2459ff0bb84658f8de192dc9aecf4e7296
mohit <mohitpardeshi729@gmail.com>
2023-05-26 12:28:11 +0530

    added authentication

--- src/components/Authentication/LogIn.tsx
+++ src/components/Authentication/LogIn.tsx
@@ -1,60 +1,131 @@
+import { useState } from 'react';
 import {
-    TextInput,
-    PasswordInput,
-    Checkbox,
-    Anchor,
-    Paper,
-    Title,
-    Text,
-    Container,
-    Group,
-    Button,
-  } from '@mantine/core';
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+} from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
-  
-  export default function AuthenticationTitle() {
-    const navigate = useNavigate();
-    
-    const handleLogIn= ()=>{
-      navigate('/dashboard')
-    }
+const URL = import.meta.env.REACT_APP_URL;
+console.log(URL)
+export default function AuthenticationTitle() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false); // Added loading state
+  const [error, setError] = useState(false); // Added error state
 
-    const handleNewAcount= ()=>{
-      navigate('/signup')
-    }
+  const handleLogIn = async () => {
+    try {
+      setLoading(true); // Set loading to true before making the request
+      const response = await fetch(`https://geeks-for-geeks-health-backend.up.railway.app/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
 
-    const handleForgotPass= ()=>{
-      navigate('/forgotpass')
-    }
+      if (response.ok) {
+        const data = await response.json();
 
-    return (
-      <Container size={420} my={40}>
-        <Title
-          align="center"
-          sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
-        >
-          Welcome back!
-        </Title>
-        <Text color="dimmed" size="sm" align="center" mt={5}>
-          Do not have an account yet?{' '}
-          <Anchor size="sm" component="button" onClick={handleNewAcount}>
-            Create account
-          </Anchor>
-        </Text>
-  
-        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-          <TextInput label="Email" placeholder="you@mantine.dev" required />
-          <PasswordInput label="Password" placeholder="Your password" required mt="md" />
-          <Group position="apart" mt="lg">
-            <Checkbox label="Remember me" />
-            <Anchor component="button" size="sm" onClick={handleForgotPass}>
-              Forgot password?
-            </Anchor>
-          </Group>
+        // Access the response data here
+        console.log(data);
+
+        // Redirect to a new page or perform other actions after successful login
+        navigate('/dashboard');
+      } else if (response.status === 500) {
+        setError(true); // Set error to true for wrong credentials
+      } else {
+        console.log('Error:', response.status);
+      }
+    } catch (error) {
+      console.log('Error:', error.message);
+    } finally {
+      setLoading(false); // Set loading back to false after the request is completed
+    }
+  };
+
+  const handleNewAcount = () => {
+    navigate('/signup');
+  };
+
+  const handleForgotPass = () => {
+    navigate('/forgotpass');
+  };
+
+  const handleInputChange = () => {
+    setError(false); // Reset error to false when input changes
+  };
+
+  return (
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
+      >
+        Welcome back!
+      </Title>
+      <Text color="dimmed" size="sm" align="center" mt={5}>
+        Do not have an account yet?{' '}
+        <Anchor size="sm" component="button" onClick={handleNewAcount}>
+          Create account
+        </Anchor>
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput
+          label="Email"
+          placeholder="abc@gmail.com"
+          required
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+            handleInputChange();
+          }}
+        />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+            handleInputChange();
+          }}
+        />
+        {error && (
+          <Text color="red" size="sm" mt="md" align="center">
+            Incorrect email or password. Please try again.
+          </Text>
+        )}
+        <Group position="apart" mt="lg">
+          <Checkbox label="Remember me" />
+        </Group>
+        {loading ? (
+          // Render loading state while waiting for the response
+          <Button fullWidth mt="xl" disabled>
+            Loading...
+          </Button>
+        ) : (
+          // Render the button to login when not loading
           <Button fullWidth mt="xl" onClick={handleLogIn}>
             Login
           </Button>
-        </Paper>
-      </Container>
-    );
-  }
+        )}
+      </Paper>
+
+      
+    </Container>
+  );
+}
--- src/components/Authentication/SignUp.tsx
+++ src/components/Authentication/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   TextInput,
   PasswordInput,
@@ -13,13 +14,76 @@ import {
 import { useNavigate } from 'react-router-dom';
 
 export default function AuthenticationTitle() {
-  const navigate = useNavigate()
-  const handleLogIn = ()=>{
-    navigate('/login')
-  }
-  const handleJoin = ()=>{
-    navigate('/quiz')
-  }
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false); // Added loading state
+  const [error, setError] = useState(false); // Added error state
+  const [errorMessage, setErrorMessage] = useState(''); // Added error message state
+
+  const handleLogIn = () => {
+    navigate('/login');
+  };
+
+  const handleJoin = async () => {
+    try {
+      setLoading(true); // Set loading to true before making the request
+
+      // Perform client-side validation
+      if (!validateEmail(email)) {
+        setError(true);
+        setErrorMessage('Invalid email format');
+        return;
+      }
+
+      if (password.length < 6) {
+        setError(true);
+        setErrorMessage('Password should be at least 6 characters long');
+        return;
+      }
+
+      const response = await fetch('https://geeks-for-geeks-health-backend.up.railway.app/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+          email,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // You can do something with the response data here
+      } else if (response.status === 500) {
+        setError(true); // Set error to true for server error
+      } else {
+        const errorMessage = await response.text(); // Get the error message from the response
+        setError(true); // Set error to true
+        setErrorMessage(errorMessage); // Set the error message
+      }
+    } catch (error) {
+      console.log('Error:', error.message);
+    } finally {
+      setLoading(false); // Set loading back to false after the request is completed
+    }
+  };
+
+  const handleInputChange = () => {
+    setError(false); // Reset error to false when input changes
+    setErrorMessage(''); // Reset error message
+  };
+
+  // Email validation function
+  const validateEmail = (email:any) => {
+    // Basic email format validation using regex
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
   return (
     <Container size={420} my={40}>
       <Title
@@ -29,19 +93,61 @@ export default function AuthenticationTitle() {
         Quit smoking, embrace freedom.!
       </Title>
       <Text color="dimmed" size="sm" align="center" mt={5}>
-        Already have an accoun?{' '}
+        Already have an account?{' '}
         <Anchor size="sm" component="button" onClick={handleLogIn}>
           Login
         </Anchor>
       </Text>
 
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="you@mantine.dev" required />
-        <TextInput label="Text" placeholder="matrix#564" required mt="md"/>
-        <PasswordInput label="Password" placeholder="Your password" required mt="md" />
-        <Button fullWidth mt="xl" onClick={handleJoin}>
-          Join Now
-        </Button>
+        <TextInput
+          label="Email"
+          placeholder="you@mantine.dev"
+          required
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+            handleInputChange();
+          }}
+        />
+        <TextInput
+          label="Username"
+          placeholder="matrix#564"
+          required
+          mt="md"
+          value={username}
+          onChange={(event) => {
+            setUsername(event.target.value);
+            handleInputChange();
+          }}
+        />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+            handleInputChange();
+          }}
+        />
+        {error && (
+          <Text color="red" size="sm" mt="md" align="center">
+            {errorMessage || 'Error occurred while joining. Please try again.'}
+          </Text>
+        )}
+        {loading ? (
+          // Render loading state while waiting for the response
+          <Button fullWidth mt="xl" disabled>
+            Loading...
+          </Button>
+        ) : (
+          // Render the button to join when not loading
+          <Button fullWidth mt="xl" onClick={handleJoin}>
+            Join Now
+          </Button>
+        )}
       </Paper>
     </Container>
   );
