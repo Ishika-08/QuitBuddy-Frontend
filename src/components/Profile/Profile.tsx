0af053f4a76613b473a29be2cbf12f795273c1f2
mohit <mohitpardeshi729@gmail.com>
2023-05-27 19:03:45 +0530

    get request working

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,31 +1,48 @@
-import "./Profile.css"
-import { useState } from 'react';
+import  { useState, useEffect } from 'react';
+import './Profile.css';
 import Navbar from './Navbar';
-import Home from "./Home/home"
-import Analytics from "./Analytics/Analytics"
+import Home from './Home/home';
+import Analytics from './Analytics/Analytics';
 import Dashboard from './Dashboard/dashboard';
-import Blogs from "./Blogs/Blogs"
+import Blogs from './Blogs/Blogs';
+import { useParams } from 'react-router-dom';
 
 const App = () => {
   const [selectedContent, setSelectedContent] = useState('Home');
+  const { userID } = useParams();
 
-  const mockData= {
-    image: "/Article/img1.jpg",
+  const mockData = {
+    image: '/Article/img1.jpg',
     link: 'https://example.com/smoking-article',
     title: 'Tips for Quitting Smoking',
-    description: 'Discover effective strategies and tips to help you quit smoking and improve your health.',
+    description:
+      'Discover effective strategies and tips to help you quit smoking and improve your health.',
     rating: '5.0',
     author: {
       name: 'John Smith',
       image: 'https://example.com/author-profile-image.jpg',
     },
   };
-  
-  
 
-  const handleNavItemSelect = (content: string) => {
+  const handleNavItemSelect = (content:any) => {
     setSelectedContent(content);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://geeks-for-geeks-health-backend.up.railway.app/${userID}/medicalData`
+        );
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [userID]);
 
   const renderSelectedContent = () => {
     switch (selectedContent) {
@@ -34,9 +51,9 @@ const App = () => {
       case 'Analytics':
         return <Analytics />;
       case 'Dashboard':
-        return <Dashboard/>;
+        return <Dashboard />;
       case 'Blogs':
-        return <Blogs {...mockData}/>
+        return <Blogs {...mockData} />;
       default:
         return null;
     }
@@ -44,12 +61,10 @@ const App = () => {
 
   return (
     <div className="container">
-        <div className="navbar">
+      <div className="navbar">
         <Navbar onNavItemSelect={handleNavItemSelect} />
-        </div>
-        <div className="content">
-        {renderSelectedContent()}
-        </div>
+      </div>
+      <div className="content">{renderSelectedContent()}</div>
     </div>
   );
 };
